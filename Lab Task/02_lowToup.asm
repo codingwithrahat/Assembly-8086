87dd58915ee79285088d7d9ecbe94006cb6f04ed
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-01-09 17:01:42 +0600

    Add assembly program to convert lowercase to uppercase

---
+++ Lab Task/02_lowToup.asm
@@ -0,0 +1,44 @@
+ .MODEL SMALL  
+ 
+ .STACK 100H
+ .DATA
+ MSG1 DB 0DH,0AH,'Enter the lowercase latter:$'
+ MSG2 DB 0DH,0AH,'The Uppercase character IS:$'
+ CHAR DB ?
+ 
+ ;new line
+ ;0DH it means CARRIAGE RETURN
+ ;0AH it meansLINE FEED
+ 
+ .CODE
+ MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    LEA DX,MSG1
+    MOV AH,9
+    INT 21H
+     
+    
+    MOV AH,1
+    INT 21H
+    SUB AL, 20H
+    MOV CHAR,AL  
+    
+     
+    LEA DX,MSG2
+    MOV AH,9
+    INT 21H
+    
+    MOV DL,CHAR
+    MOV AH,2
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
+    
+
+ 
