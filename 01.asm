23e4a4e7c4bbe58998ff45fa813499e51390c203
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-01-09 11:32:56 +0600

    Create 01.asm

---
+++ 01.asm
@@ -0,0 +1,22 @@
+.MODEL SMALL
+.STACK 100H
+.CODE
+
+MAIN PROC
+       
+    ;INPUT
+    MOV AH, 1
+    INT 21H
+    MOV BL, AL
+       
+    ;OUTPUT
+    MOV AH, 2
+    MOV DL, BL
+    INT 21H
+        
+    ;ENP RPOGRAM
+    EXIT:
+    MOV AH, 4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
