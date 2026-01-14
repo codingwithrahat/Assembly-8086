e934065b35fc3fe26398cbc3b07b0090539b4df1
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-01-14 15:55:22 +0600

    Create B_12_evenAnd.asm

---
+++ Practice Problems/B_12_evenAnd.asm
@@ -0,0 +1,49 @@
+;Problem 12: Even or Odd using AND Logic 
+;Question: Input a digit and use AND operation to check if it's even or odd. Display "EVEN NUMBER!" or "ODD NUMBER!".
+ 
+.MODEL SMALL
+.STACK 100H
+.DATA
+M1 DB 'ENTER A DIGIT: $'
+M2 DB 'EVEN NUMBER!$'
+M3 DB 'ODD NUMBER!$'
+
+
+.CODE
+MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    LEA DX,M1
+    MOV AH,9
+    INT 21H
+    
+    MOV AH,1
+    INT 21H 
+    SUB AL, 30H
+    
+    AND AL, 1 
+    
+    CMP AL, 0
+    JE EV  
+    
+    LEA DX,M3
+    
+    JMP DISPLAY
+    
+     
+    EV: 
+    LEA DX,M2
+    
+    DISPLAY:
+    MOV AH,9
+    INT 21H
+       
+    
+    EXIT:
+    MOV AH, 4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
+    
+     
