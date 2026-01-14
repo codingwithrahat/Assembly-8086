559e143c080d0583794fcc632f3d4d27794d973f
Mohammad Rahat Hossan <131850941+codingwithrahat@users.noreply.github.com>
2026-01-14 12:56:39 +0600

    Create 05.average.asm

---
+++ Lab Task/05.average.asm
@@ -0,0 +1,74 @@
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+MSG1 DB 0DH,0AH,'Enter First Number:$'
+MSG2 DB 0DH,0AH,'Enter Second Number:$'
+MSG4 DB 0DH,0AH,'Enter Third Number:$'
+MSG3 DB 0DH,0AH,'AVERAGE : $'
+
+NUM1  DB ?
+NUM2  DB ?
+NUM3  DB ?
+SUM   DB ?
+THREE DB 3
+
+.CODE
+MAIN PROC  
+    
+    
+    MOV AX, @DATA
+    MOV DS, AX
+
+
+    LEA DX, MSG1
+    MOV AH, 9
+    INT 21H
+
+    MOV AH, 1
+    INT 21H
+    SUB AL, 30H
+    MOV NUM1, AL
+
+    LEA DX, MSG2
+    MOV AH, 9
+    INT 21H
+
+    MOV AH, 1
+    INT 21H
+    SUB AL, 30H
+    MOV NUM2, AL
+
+
+    LEA DX, MSG4
+    MOV AH, 9
+    INT 21H
+
+    MOV AH, 1
+    INT 21H
+    SUB AL, 30H
+    MOV NUM3, AL 
+    
+
+    MOV AL, NUM1
+    ADD AL, NUM2
+    ADD AL, NUM3
+    MOV AH, 0
+    DIV THREE          
+
+    MOV SUM, AL
+    ADD SUM, 30H       
+
+    LEA DX, MSG3
+    MOV AH, 9
+    INT 21H
+
+    MOV DL, SUM
+    MOV AH, 2
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
